c39a432d49c850ff159818e1032267dc0fe3bddf
ValentinCheco <104083777+ValentinCheco@users.noreply.github.com>
2022-07-06 17:43:34 -0300

    Add files via upload

---
+++ Trabajo Final/js/javascript.js
@@ -0,0 +1,10 @@
+function AmpliarImg (x) {x.style.height = "100%";
+x.style.width = "101%"}
+
+function NormalImg(x) {
+  x.style.height = "100%";
+  x.style.width = "100%"
+
+}
+function AmpliarImg1 (x) {x.style.height = "100%";
+x.style.width = "101%"}
